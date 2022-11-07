3f7259786be7b805ff465b32d597a666b17685c3
Thibault Duplessis <t@lichess.org>
2022-11-08 00:22:04 +0100

    comment autoconfig impl

--- core/play/src/main/scala/play/api/autoconfig/AutoConfigImpl.scala
+++ core/play/src/main/scala/play/api/autoconfig/AutoConfigImpl.scala
@@ -16,53 +16,53 @@
 
 package io.methvin.play.autoconfig
 
-import scala.reflect.macros.blackbox
+// import scala.reflect.macros.blackbox
 
-private[autoconfig] class AutoConfigImpl(val c: blackbox.Context) {
-  import c.universe._
+// private[autoconfig] class AutoConfigImpl(val c: blackbox.Context) {
+//   import c.universe._
 
-  def loader[T](implicit t: WeakTypeTag[T]): Tree = {
-    val tpe = t.tpe
-    // Look at all constructors
-    val constructors: Seq[MethodSymbol] = tpe.members.collect {
-      case m if m.isConstructor && !m.fullName.endsWith("$init$") =>
-        m.asMethod
-    }.toSeq
+//   def loader[T](implicit t: WeakTypeTag[T]): Tree = {
+//     val tpe = t.tpe
+//     // Look at all constructors
+//     val constructors: Seq[MethodSymbol] = tpe.members.collect {
+//       case m if m.isConstructor && !m.fullName.endsWith("$init$") =>
+//         m.asMethod
+//     }.toSeq
 
-    val configConstructors = constructors.filter(_.annotations.exists(_.tree.tpe =:= typeOf[ConfigConstructor]))
-    val constructor = configConstructors match {
-      case Seq() =>
-        // Find the primary constructor
-        constructors
-          .find(_.isPrimaryConstructor)
-          .getOrElse(c.abort(c.enclosingPosition, s"No primary constructor found!"))
-      case Seq(cons) =>
-        cons
-      case _ =>
-        // multiple annotated constructors found
-        c.abort(c.enclosingPosition, s"Multiple constructors found annotated with @ConfigConstructor")
-    }
+//     val configConstructors = constructors.filter(_.annotations.exists(_.tree.tpe =:= typeOf[ConfigConstructor]))
+//     val constructor = configConstructors match {
+//       case Seq() =>
+//         // Find the primary constructor
+//         constructors
+//           .find(_.isPrimaryConstructor)
+//           .getOrElse(c.abort(c.enclosingPosition, s"No primary constructor found!"))
+//       case Seq(cons) =>
+//         cons
+//       case _ =>
+//         // multiple annotated constructors found
+//         c.abort(c.enclosingPosition, s"Multiple constructors found annotated with @ConfigConstructor")
+//     }
 
-    val confTerm = TermName(c.freshName("conf$"))
-    val argumentLists = constructor.paramLists.map { params =>
-      params.map { p =>
-        val name = p.annotations
-          .collectFirst {
-            case ann if ann.tree.tpe =:= typeOf[ConfigName] =>
-              ann.tree.children.collectFirst { case Literal(Constant(str: String)) => str }.get
-          }
-          .getOrElse(p.name.decodedName.toString)
-        q"implicitly[_root_.play.api.ConfigLoader[${p.typeSignature}]].load($confTerm, $name)"
-      }
-    }
-    q"""
-      new _root_.play.api.ConfigLoader[$tpe] {
-        override def load(config: _root_.com.typesafe.config.Config, path: String) = {
-          val $confTerm = if (path.isEmpty) config else config.getConfig(path)
-          new $tpe(...$argumentLists)
-        }
-      }
-    """
-  }
+//     val confTerm = TermName(c.freshName("conf$"))
+//     val argumentLists = constructor.paramLists.map { params =>
+//       params.map { p =>
+//         val name = p.annotations
+//           .collectFirst {
+//             case ann if ann.tree.tpe =:= typeOf[ConfigName] =>
+//               ann.tree.children.collectFirst { case Literal(Constant(str: String)) => str }.get
+//           }
+//           .getOrElse(p.name.decodedName.toString)
+//         q"implicitly[_root_.play.api.ConfigLoader[${p.typeSignature}]].load($confTerm, $name)"
+//       }
+//     }
+//     q"""
+//       new _root_.play.api.ConfigLoader[$tpe] {
+//         override def load(config: _root_.com.typesafe.config.Config, path: String) = {
+//           val $confTerm = if (path.isEmpty) config else config.getConfig(path)
+//           new $tpe(...$argumentLists)
+//         }
+//       }
+//     """
+//   }
 
-}
+// }
