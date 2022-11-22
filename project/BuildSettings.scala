3cc08ad7e2495177548f4220e34b74ee9aff09db
Thibault Duplessis <t@lichess.org>
2022-11-22 17:01:21 +0100

    make sure nested routers are in the router package

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -18,7 +18,7 @@ import scala.util.control.NonFatal
 
 object BuildSettings {
 
-  val playVersion = "2.8.18-lila_1.20"
+  val playVersion = "2.8.18-lila_1.21"
 
   /** File header settings.  */
   private def fileUriRegexFilter(pattern: String): FileFilter = new FileFilter {
