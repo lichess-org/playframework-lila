842e759efce4f3d8c67d07f915ebbf11d8663447
Thibault Duplessis <t@lichess.org>
2024-03-29 08:56:46 +0100

    bump route compiler version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -18,7 +18,7 @@ import scala.util.control.NonFatal
 
 object BuildSettings {
 
-  val playVersion = "2.8.18-lila_1.21"
+  val playVersion = "2.8.18-lila_1.22"
 
   /** File header settings.  */
   private def fileUriRegexFilter(pattern: String): FileFilter = new FileFilter {
